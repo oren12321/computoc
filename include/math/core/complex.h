4ef51d19525cd94af0376924af234d21240f622f
oren12321 <oren12321@gmail.com>
2022-04-29 15:01:14 +0300

    Set Complex underlying type to Decimal only

--- include/math/core/complex.h
+++ include/math/core/complex.h
@@ -9,95 +9,95 @@
 
 namespace math::core::types {
     template <typename T>
-    concept Number = (std::is_integral_v<T> && std::is_signed_v<T> && !std::is_same_v<T, bool>) || std::is_floating_point_v<T>;
+    concept Decimal = std::is_floating_point_v<T>;
 
-    template <Number N>
+    template <Decimal F>
     class Complex {
     public:
-        Complex(N r = N{ 0 }, N i = N{ 0 })
+        Complex(F r = F{ 0 }, F i = F{ 0 })
             : r_(r), i_(i) {}
 
-        N r() const noexcept
+        F r() const noexcept
         {
             return r_;
         }
 
-        N i() const noexcept
+        F i() const noexcept
         {
             return i_;
         }
 
-        template <Number N_o>
-        friend bool operator==(const Complex<N_o>& lhs, const Complex<N_o>& rhs) noexcept;
+        template <Decimal F_o>
+        friend bool operator==(const Complex<F_o>& lhs, const Complex<F_o>& rhs) noexcept;
 
-        operator N() const noexcept
+        operator F() const noexcept
         {
             return r_;
         }
 
-        Complex<N> operator-() const noexcept
+        Complex<F> operator-() const noexcept
         {
             return { -r_, -i_ };
         }
 
-        Complex<N> conjugate() const noexcept
+        Complex<F> conjugate() const noexcept
         {
             return { r_, -i_ };
         }
 
-        template <Number N_o>
-        friend Complex<N_o> operator+(const Complex<N_o>& lhs, const Complex<N_o>& rhs) noexcept;
+        template <Decimal F_o>
+        friend Complex<F_o> operator+(const Complex<F_o>& lhs, const Complex<F_o>& rhs) noexcept;
 
-        Complex<N>& operator+=(const Complex<N>& other) noexcept
+        Complex<F>& operator+=(const Complex<F>& other) noexcept
         {
             r_ += other.r_;
             i_ += other.i_;
             return *this;
         }
 
-        template <Number N_o>
-        friend Complex<N_o> operator-(const Complex<N_o>& lhs, const Complex<N_o>& rhs) noexcept;
+        template <Decimal F_o>
+        friend Complex<F_o> operator-(const Complex<F_o>& lhs, const Complex<F_o>& rhs) noexcept;
 
-        Complex<N>& operator-=(const Complex<N>& other) noexcept
+        Complex<F>& operator-=(const Complex<F>& other) noexcept
         {
             r_ -= other.r_;
             i_ -= other.i_;
             return *this;
         }
 
-        template <Number N_o>
-        friend Complex<N_o> operator*(const Complex<N_o>& lhs, const Complex<N_o>& rhs) noexcept;
+        template <Decimal F_o>
+        friend Complex<F_o> operator*(const Complex<F_o>& lhs, const Complex<F_o>& rhs) noexcept;
 
-        Complex<N>& operator*=(const Complex<N>& other) noexcept
+        Complex<F>& operator*=(const Complex<F>& other) noexcept
         {
-            N nr{r_ * other.r_ - i_ * other.i_};
-            N ni{r_ * other.i_ + other.r_ * i_};
+            F nr{r_ * other.r_ - i_ * other.i_};
+            F ni{r_ * other.i_ + other.r_ * i_};
             r_ = nr;
             i_ = ni;
             return *this;
         }
 
-        Complex<N> multiplicative_inverse() const
+        Complex<F> multiplicative_inverse() const
         {
             CORE_EXPECT(r_ != 0 || i_ != 0, std::overflow_error, "division by zero");
 
             return {r_ / (r_ * r_ + i_ * i_), -i_ / (r_ * r_ + i_ * i_)};
         }
 
-        template <Number N_o>
-        friend Complex<N_o> operator/(const Complex<N_o>& lhs, const Complex<N_o>& rhs);
+        template <Decimal F_o>
+        friend Complex<F_o> operator/(const Complex<F_o>& lhs, const Complex<F_o>& rhs);
 
-        Complex<N>& operator/=(const Complex<N>& other)
+        Complex<F>& operator/=(const Complex<F>& other)
         {
             return operator*=(other.multiplicative_inverse());
         }
 
-        N squared_magnitude() const noexcept
+        F squared_magnitude() const noexcept
         {
             return r_ * r_ + i_ * i_;
         }
 
-        N theta() const
+        F theta() const
         {
             CORE_EXPECT(r_ != 0, std::overflow_error, "division by zero");
 
@@ -105,36 +105,36 @@ namespace math::core::types {
         }
 
     private:
-        N r_{ 0 };
-        N i_{ 0 };
+        F r_{ 0 };
+        F i_{ 0 };
     };
 
-    template <Number N>
-    inline bool operator==(const Complex<N>& lhs, const Complex<N>& rhs) noexcept
+    template <Decimal F>
+    inline bool operator==(const Complex<F>& lhs, const Complex<F>& rhs) noexcept
     {
         return lhs.r_ == rhs.r_ && lhs.i_ == rhs.i_;
     }
 
-    template <Number N>
-    inline Complex<N> operator+(const Complex<N>& lhs, const Complex<N>& rhs) noexcept
+    template <Decimal F>
+    inline Complex<F> operator+(const Complex<F>& lhs, const Complex<F>& rhs) noexcept
     {
         return {lhs.r_ + rhs.r_, lhs.i_ + rhs.i_};
     }
 
-    template <Number N>
-    inline Complex<N> operator-(const Complex<N>& lhs, const Complex<N>& rhs) noexcept
+    template <Decimal F>
+    inline Complex<F> operator-(const Complex<F>& lhs, const Complex<F>& rhs) noexcept
     {
         return {lhs.r_ - rhs.r_, lhs.i_ - rhs.i_};
     }
 
-    template <Number N>
-    inline Complex<N> operator*(const Complex<N>& lhs, const Complex<N>& rhs) noexcept
+    template <Decimal F>
+    inline Complex<F> operator*(const Complex<F>& lhs, const Complex<F>& rhs) noexcept
     {
         return {lhs.r_ * rhs.r_ - lhs.i_ * rhs.i_, lhs.r_ * rhs.i_ + rhs.r_ * lhs.i_};
     }
 
-    template <Number N>
-    inline Complex<N> operator/(const Complex<N>& lhs, const Complex<N>& rhs)
+    template <Decimal F>
+    inline Complex<F> operator/(const Complex<F>& lhs, const Complex<F>& rhs)
     {
         return operator*(lhs, rhs.multiplicative_inverse());
     }
--- test/complex.cpp
+++ test/complex.cpp
@@ -9,30 +9,30 @@ TEST(Complex_test, can_be_initialized_with_components_or_a_number)
 {
     using namespace math::core::types;
 
-    Complex c1 = 0;
-    EXPECT_EQ(0, c1.r());
-    EXPECT_EQ(0, c1.i());
+    Complex c1 = 0.0;
+    EXPECT_EQ(0.0, c1.r());
+    EXPECT_EQ(0.0, c1.i());
 
     Complex c2 = 0.5;
     EXPECT_EQ(0.5, c2.r());
     EXPECT_EQ(0.0, c2.i());
 
-    Complex c3{ 0, 1 };
-    EXPECT_EQ(0, c3.r());
-    EXPECT_EQ(1, c3.i());
+    Complex c3{ 0.0, 1.0 };
+    EXPECT_EQ(0.0, c3.r());
+    EXPECT_EQ(1.0, c3.i());
 
-    Complex c4 = { 1, 2 };
-    EXPECT_EQ(1, c4.r());
-    EXPECT_EQ(2, c4.i());
+    Complex c4 = { 1.0, 2.0 };
+    EXPECT_EQ(1.0, c4.r());
+    EXPECT_EQ(2.0, c4.i());
 }
 
 TEST(Complex_test, can_be_compared_with_other_number)
 {
     using namespace math::core::types;
 
-    EXPECT_EQ(0, (Complex{ 0 }));
-    EXPECT_EQ((Complex{ 1, 2 }), (Complex{ 1, 2 }));
-    EXPECT_EQ((Complex{ 0, 1 }), (Complex{ 0, 1 }));
+    EXPECT_EQ(0.0, (Complex{ 0.0 }));
+    EXPECT_EQ((Complex{ 1.0, 2.0 }), (Complex{ 1.0, 2.0 }));
+    EXPECT_EQ((Complex{ 0.0, 1.0 }), (Complex{ 0.0, 1.0 }));
     EXPECT_EQ(0.5, (Complex{ 0.5 }));
 }
 
@@ -40,9 +40,9 @@ TEST(Complex_test, can_negate)
 {
     using namespace math::core::types;
 
-    EXPECT_EQ(0, (-Complex{ 0 }));
-    EXPECT_EQ((Complex{ -1, -2 }), (-Complex{ 1, 2 }));
-    EXPECT_EQ((Complex{ 0, -1 }), (-Complex{ 0, 1 }));
+    EXPECT_EQ(0.0, (-Complex{ 0.0 }));
+    EXPECT_EQ((Complex{ -1.0, -2.0 }), (-Complex{ 1.0, 2.0 }));
+    EXPECT_EQ((Complex{ 0.0, -1.0 }), (-Complex{ 0.0, 1.0 }));
     EXPECT_EQ(-0.5, (-Complex{ 0.5 }));
 }
 
@@ -50,9 +50,9 @@ TEST(Complex_test, have_conjugate)
 {
     using namespace math::core::types;
 
-    EXPECT_EQ(0, (Complex{ 0 }.conjugate()));
-    EXPECT_EQ((Complex{ 1, -2 }), (Complex{ 1, 2 }.conjugate()));
-    EXPECT_EQ((Complex{ 0, -1 }), (Complex{ 0, 1 }.conjugate()));
+    EXPECT_EQ(0.0, (Complex{ 0.0 }.conjugate()));
+    EXPECT_EQ((Complex{ 1.0, -2.0 }), (Complex{ 1.0, 2.0 }.conjugate()));
+    EXPECT_EQ((Complex{ 0.0, -1.0 }), (Complex{ 0.0, 1.0 }.conjugate()));
     EXPECT_EQ(0.5, (Complex{ 0.5 }.conjugate()));
 }
 
@@ -60,49 +60,49 @@ TEST(Complex_test, can_be_added_to_other_number)
 {
     using namespace math::core::types;
 
-    Complex c{1, 2};
-    EXPECT_EQ(c, c + 0);
-    EXPECT_EQ((Complex{3, 2}), c + 2);
+    Complex c{1.0, 2.0};
+    EXPECT_EQ(c, c + 0.0);
+    EXPECT_EQ((Complex{3.0, 2.0}), c + 2.0);
     EXPECT_EQ((Complex{1.5, 2.0}), c + 0.5);
-    EXPECT_EQ((Complex{2, 1}), (c + Complex{1, -1}));
+    EXPECT_EQ((Complex{2.0, 1.0}), (c + Complex{1.0, -1.0}));
 
-    c += Complex{1, -1};
-    EXPECT_EQ((Complex{2, 1}), c);
+    c += Complex{1.0, -1.0};
+    EXPECT_EQ((Complex{2.0, 1.0}), c);
 }
 
 TEST(Complex_test, can_be_subtracted_from_other_number)
 {
     using namespace math::core::types;
 
-    Complex c{1, 2};
-    EXPECT_EQ(c, c - 0);
-    EXPECT_EQ((Complex{-1, 2}), c - 2);
+    Complex c{1.0, 2.0};
+    EXPECT_EQ(c, c - 0.0);
+    EXPECT_EQ((Complex{-1.0, 2.0}), c - 2.0);
     EXPECT_EQ((Complex{0.5, 2.0}), c - 0.5);
-    EXPECT_EQ((Complex{0, 3}), (c - Complex{1, -1}));
+    EXPECT_EQ((Complex{0.0, 3.0}), (c - Complex{1.0, -1.0}));
 
-    c -= Complex{1, -1};
-    EXPECT_EQ((Complex{0, 3}), c);
+    c -= Complex{1.0, -1.0};
+    EXPECT_EQ((Complex{0.0, 3.0}), c);
 }
 
 TEST(Complex_test, can_be_multiplied_with_other_number)
 {
     using namespace math::core::types;
 
-    Complex c{6, -2};
-    EXPECT_EQ(0, c * 0);
-    EXPECT_EQ(c, c * 1);
+    Complex c{6.0, -2.0};
+    EXPECT_EQ(0.0, c * 0.0);
+    EXPECT_EQ(c, c * 1.0);
     EXPECT_EQ((Complex{1.5, -0.5}), c * 0.25);
-    EXPECT_EQ((Complex{30, 10}), (c * Complex{4, 3}));
+    EXPECT_EQ((Complex{30.0, 10.0}), (c * Complex{4.0, 3.0}));
 
-    c *= Complex{4, 3};
-    EXPECT_EQ((Complex{30, 10}), c);
+    c *= Complex{4.0, 3.0};
+    EXPECT_EQ((Complex{30.0, 10.0}), c);
 }
 
 TEST(Complex_test, have_multiplicate_reciprocal)
 {
     using namespace math::core::types;
 
-    EXPECT_THROW((Complex{0}.multiplicative_inverse()), std::overflow_error);
+    EXPECT_THROW((Complex{0.0}.multiplicative_inverse()), std::overflow_error);
 
     EXPECT_EQ((Complex{3.0 / 25.0, -4.0 / 25.0}), (Complex{3.0, 4.0}.multiplicative_inverse()));
 }
@@ -124,18 +124,18 @@ TEST(Complex_test, have_squared_magnitude)
 {
     using namespace math::core::types;
 
-    EXPECT_EQ(0, (Complex{ 0 }.squared_magnitude()));
-    EXPECT_EQ(2, (Complex{ 1, 1 }.squared_magnitude()));
-    EXPECT_EQ(5, (Complex{ 1, 2 }.squared_magnitude()));
-    EXPECT_EQ(4, (Complex{ 0, 2 }.squared_magnitude()));
+    EXPECT_EQ(0.0, (Complex{ 0.0 }.squared_magnitude()));
+    EXPECT_EQ(2.0, (Complex{ 1.0, 1.0 }.squared_magnitude()));
+    EXPECT_EQ(5.0, (Complex{ 1.0, 2.0 }.squared_magnitude()));
+    EXPECT_EQ(4.0, (Complex{ 0.0, 2.0 }.squared_magnitude()));
 }
 
 TEST(Complex_test, have_angle)
 {
     using namespace math::core::types;
 
-    EXPECT_THROW((Complex{ 0 }.theta()), std::overflow_error);
+    EXPECT_THROW((Complex{ 0.0 }.theta()), std::overflow_error);
 
-    EXPECT_EQ(0, (Complex{ 1, 0 }.theta()));
+    EXPECT_EQ(0.0, (Complex{ 1.0, 0.0 }.theta()));
     EXPECT_EQ(std::numbers::pi / 4.0, (Complex{ 1.0, 1.0 }.theta()));
 }
