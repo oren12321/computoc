3b5150537a58176fdf4722ffc385b7500d687a9e
oren12321 <oren12321@gmail.com>
2022-11-24 15:49:53 +0200

    Add tests to MEMOCPP_THROW_IF_FALSE macro and add alias for it in computoc library

--- include/computoc/errors.h
+++ include/computoc/errors.h
@@ -4,5 +4,6 @@
 #include <memoc/errors.h>
 
 #define COMPUTOC_THROW_IF_FALSE(condition,exception_type,...) MEMOC_THROW_IF_FALSE(condition,exception_type,__VA_ARGS__)
+#define COMPUTOCPP_THROW_IF_FALSE(condition,exception_type,...) MEMOCPP_THROW_IF_FALSE(condition,exception_type,__VA_ARGS__)
 
 #endif // COMPUTOC_ERRORS_H
