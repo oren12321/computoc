3a596e966c8b4988f8c28609a03d69476a6df0ad
oren12321 <oren12321@gmail.com>
2022-12-26 01:43:37 +0200

    Add Interval type to computoc

--- include/computoc/utils.h
+++ include/computoc/utils.h
@@ -28,12 +28,57 @@ namespace computoc {
         {
             return ((value % modulus) + modulus) % modulus;
         }
+
+        template <Number T = std::int64_t>
+        struct Interval {
+            Interval(const T& nstart, const T& nstop, const T& nstep) noexcept
+                : start(nstart), stop(nstop), step(nstep) {}
+
+            Interval(const T& nstart, const T& nstop) noexcept
+                : Interval(nstart, nstop, 1) {}
+
+            Interval(const T& nstart) noexcept
+                : Interval(nstart, nstart, 1) {}
+
+            Interval() = default;
+            Interval(const Interval&) = default;
+            Interval& operator=(const Interval&) = default;
+            Interval(Interval&) = default;
+            Interval& operator=(Interval&) = default;
+
+            T start{ 0 };
+            T stop{ 0 };
+            T step{ 1 };
+        };
+
+        template <Number T>
+        inline Interval<T> reverse(const Interval<T>& i) noexcept
+        {
+            return { i.stop, i.start, -i.step };
+        }
+
+        template <Number T>
+        inline Interval<T> modulo(const Interval<T>& i, const T& modulus) noexcept
+        {
+            return { modulo(i.start, modulus), modulo(i.stop, modulus), i.step };
+        }
+
+        template <Number T>
+        inline Interval<T> forward(const Interval<T>& i) noexcept
+        {
+            return i.step < T{ 0 } ? reverse(i) : i;
+        }
     }
 
     using details::Params;
 
+    using details::Interval;
+
     using details::close;
     using details::modulo;
+
+    using details::reverse;
+    using details::forward;
 }
 
 #endif
