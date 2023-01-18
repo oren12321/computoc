086620c593b1949382dd8980125a9cb0f87f5a79
oren12321 <oren12321@gmail.com>
2023-01-19 00:18:21 +0200

    rename enum_to_string function to to_string

--- test/erroc/enumgen.cpp
+++ test/erroc/enumgen.cpp
@@ -12,12 +12,12 @@ TEST(Enumgen_test, generate_enum_with_matching_strings)
 {
     EXPECT_EQ(4, static_cast<int>(Errors::size));
 
-    EXPECT_STREQ("division_by_zero", enum_to_string(Errors::division_by_zero));
-    EXPECT_STREQ("invalid_argument", enum_to_string(Errors::invalid_argument));
-    EXPECT_STREQ("file_not_found", enum_to_string(Errors::file_not_found));
-    EXPECT_STREQ("runtime_error", enum_to_string(Errors::runtime_error));
+    EXPECT_STREQ("division_by_zero", to_string(Errors::division_by_zero));
+    EXPECT_STREQ("invalid_argument", to_string(Errors::invalid_argument));
+    EXPECT_STREQ("file_not_found", to_string(Errors::file_not_found));
+    EXPECT_STREQ("runtime_error", to_string(Errors::runtime_error));
 
     Errors error{ Errors::division_by_zero };
 
-    EXPECT_STREQ("division_by_zero", enum_to_string(error));
+    EXPECT_STREQ("division_by_zero", to_string(error));
 }
