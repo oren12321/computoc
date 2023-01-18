f98a6f00149d38741342c27fa67698710565cb0d
oren12321 <oren12321@gmail.com>
2023-01-18 12:30:59 +0200

    Add enum_to_string function

--- test/erroc/enumgen.cpp
+++ test/erroc/enumgen.cpp
@@ -2,18 +2,22 @@
 
 #include <erroc/enumgen.h>
 
+GENERATE_ENUM(Errors,
+    division_by_zero,
+    invalid_argument,
+    file_not_found,
+    runtime_error);
+
 TEST(Enumgen_test, generate_enum_with_matching_strings)
 {
-    GENERATE_ENUM(Errors,
-        division_by_zero,
-        invalid_argument,
-        file_not_found,
-        runtime_error);
-
     EXPECT_EQ(4, static_cast<int>(Errors::size));
 
-    EXPECT_STREQ("division_by_zero", ENUM_TO_CSTRING(Errors, division_by_zero));
-    EXPECT_STREQ("invalid_argument", ENUM_TO_CSTRING(Errors, invalid_argument));
-    EXPECT_STREQ("file_not_found", ENUM_TO_CSTRING(Errors, file_not_found));
-    EXPECT_STREQ("runtime_error", ENUM_TO_CSTRING(Errors, runtime_error));
+    EXPECT_STREQ("division_by_zero", enum_to_string(Errors::division_by_zero));
+    EXPECT_STREQ("invalid_argument", enum_to_string(Errors::invalid_argument));
+    EXPECT_STREQ("file_not_found", enum_to_string(Errors::file_not_found));
+    EXPECT_STREQ("runtime_error", enum_to_string(Errors::runtime_error));
+
+    Errors error{ Errors::division_by_zero };
+
+    EXPECT_STREQ("division_by_zero", enum_to_string(error));
 }
