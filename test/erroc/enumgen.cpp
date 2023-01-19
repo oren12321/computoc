1fc5fc0e888c919e2734605e4a214386e9bef574
oren12321 <oren12321@gmail.com>
2023-01-19 23:57:29 +0200

    Add enum name to enum strings in GENERATE_ENUM macro

--- test/erroc/enumgen.cpp
+++ test/erroc/enumgen.cpp
@@ -12,12 +12,12 @@ TEST(Enumgen_test, generate_enum_with_matching_strings)
 {
     EXPECT_EQ(4, static_cast<int>(Errors::size));
 
-    EXPECT_STREQ("division_by_zero", to_string(Errors::division_by_zero));
-    EXPECT_STREQ("invalid_argument", to_string(Errors::invalid_argument));
-    EXPECT_STREQ("file_not_found", to_string(Errors::file_not_found));
-    EXPECT_STREQ("runtime_error", to_string(Errors::runtime_error));
+    EXPECT_STREQ("Errors:: (division_by_zero)", to_string(Errors::division_by_zero));
+    EXPECT_STREQ("Errors:: (invalid_argument)", to_string(Errors::invalid_argument));
+    EXPECT_STREQ("Errors:: (file_not_found)", to_string(Errors::file_not_found));
+    EXPECT_STREQ("Errors:: (runtime_error)", to_string(Errors::runtime_error));
 
     Errors error{ Errors::division_by_zero };
 
-    EXPECT_STREQ("division_by_zero", to_string(error));
+    EXPECT_STREQ("Errors:: (division_by_zero)", to_string(error));
 }
